09f159b5c60ad3deff157bf9960cdff0e49cc207
wiktorlemanski <wiktor.lemanski@kainos.com>
2026-08-18 15:21:43 +0200

    test framework fully prepared

--- e2e/api/HealthApiClient.ts
+++ e2e/api/HealthApiClient.ts
@@ -3,7 +3,7 @@ import type { APIRequestContext, APIResponse } from "@playwright/test";
 export class HealthApiClient {
   constructor(private readonly request: APIRequestContext) {}
 
-  public async getHealth(): Promise<APIResponse> {
+  public getHealth(): Promise<APIResponse> {
     return this.request.get("/health");
   }
 }
